97291cb633d8145c1d9d84b7ebc3fb8e5b367591
Ramyanil Raha <rammyanil2000@gmail.com>
2022-07-02 12:38:55 +0530

    [DEL]Reduntant Files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Team Tech Stark</h1>
+      <h3>Smart India Hackathon 2022</h3>
     </div>
   );
 }
