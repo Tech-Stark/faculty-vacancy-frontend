d8ac3c1fdfff0d314a855370d48e0232fee301c1
Ayush Pratap Singh <apsingh1843@gmail.com>
2022-07-02 14:33:15 +0530

    mui and icons setup

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,6 @@
 import { useSelector } from "react-redux";
+import Button from '@mui/material/Button';
+import EmojiPeopleIcon from '@mui/icons-material/EmojiPeople';
 
 const Home = () => {
   const { isLoggedIn } = useSelector((state) => state.auth);
@@ -7,6 +9,8 @@ const Home = () => {
       <h1>Team Tech Stark</h1>
       <h3>Smart India Hackathon 2022</h3>
       <h6>Login Status : {isLoggedIn ? 'Logged in' : 'Not logged in'} </h6>
+      <Button variant="contained">Hello World</Button>
+      <EmojiPeopleIcon />
     </div>
   );
 };
