9aed1b85aae60c49eb5f649711dbeea098e0ac54
Ayush Pratap Singh <apsingh1843@gmail.com>
2022-08-26 16:49:40 +0530

    admin redirect

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,11 +2,14 @@ import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
 const Home = () => {
-  const { isLoggedIn, isAdmin } = useSelector((state) => state.auth);
+  const { isLoggedIn, isAdmin, isSuperAdmin } = useSelector((state) => state.auth);
 
   if (isLoggedIn && !isAdmin) {
     return <Navigate to="/vacancy" replace />;
   } else if (isLoggedIn && isAdmin) {
+    return <Navigate to="/admin/vacancy" replace />;
+  }
+  else if(isLoggedIn && isSuperAdmin) {
     return <Navigate to="/admin/vacancy" replace />;
   }
 
